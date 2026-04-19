a6c858de2bbb610f88f0fad2d87c20353ff22468
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-04-19 23:48:39 +0600

    Update practice.c

--- practice.c
+++ practice.c
@@ -102,3 +102,37 @@ printf("%d",ava/n);
 return 0;
 }
 
+
+
+
+
+
+
+//Find the largest element in an array.
+#include<stdio.h>
+int main(){
+int n;
+scanf("%d",&n);
+int array[n];
+for(int i = 0;i<n;i++){
+    scanf("%d",&array[i]);
+}
+int large;
+large = array[0];
+for (int i = 0;i<n;i++){
+  if(array[i]>large){
+    large = array[i];
+  }
+}
+
+printf("%d",large);
+
+
+
+
+
+
+
+return 0;
+}
+
