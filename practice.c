aad2fa74245df564cff06a1d13b0ae6b02412b85
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-04-19 23:48:57 +0600

    Update practice.c

--- practice.c
+++ practice.c
@@ -136,3 +136,37 @@ printf("%d",large);
 return 0;
 }
 
+
+
+
+
+
+
+//Find the smallest element in an array.
+#include<stdio.h>
+int main(){
+int n;
+scanf("%d",&n);
+int array[n];
+for(int i = 0;i<n;i++){
+    scanf("%d",&array[i]);
+}
+int small;
+small = array[0];
+for (int i = 0;i<n;i++){
+  if(array[i]<small){
+    small = array[i];
+  }
+}
+
+printf("%d",small);
+
+
+
+
+
+
+
+return 0;
+}
+
